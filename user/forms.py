e2cb5a0b1690db8fa84544084947060ac9deba6b
Jehan Hasan <jhasan0122@gmail.com>
2024-12-09 20:08:43 +0600

    prediction in the profile by user

--- user/forms.py
+++ user/forms.py
@@ -23,5 +23,8 @@ class UserUpdateForm(forms.ModelForm):
 class PatientProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Patient
-        fields = ['image','bio','gender','height','weight','location','phone_number','activity_level','dob']
+        fields = ['image','bio','gender','height','weight','location','phone_number','activity_level','dob','bmi','bmr']
+
+    bmi = forms.FloatField(required=False)
+    bmr = forms.FloatField(required=False)
 
